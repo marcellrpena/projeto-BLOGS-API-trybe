a0b6264bdc8ad98ca5ab07041817b46b0c41ff66
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-10-25 19:58:24 -0300

    concluído o requisito 01

---
+++ src/migrations/20221025220047-blog_posts.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE 
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE 
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
